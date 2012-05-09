32c21d101655179f1babc4c34a1143b56e018056
Tomas Forsyth Rosin <tomasrosin@gmail.com>
2012-05-09 13:33:50 +0200

    small updates

--- sandbox/threeTest/src/calculus/scheduler.js
+++ sandbox/threeTest/src/calculus/scheduler.js
@@ -1,3 +1,5 @@
+CALC = {};
+
 CALC.scheduler = function() {
 	
 	var nextId = 0;
@@ -5,6 +7,7 @@ CALC.scheduler = function() {
 	var that = {};
 
 	that.attach = function(fun, args, delay) {
+
 		delay = delay || 0;
 		events[nextId] = {
 			fun: fun,
@@ -23,6 +26,7 @@ CALC.scheduler = function() {
 	};
 
 	that.animate = function() {
+	
 		var ev;
 		
 		//Copy all events, to prevent events queued in current event loop to be executed
@@ -32,18 +36,19 @@ CALC.scheduler = function() {
 				currentEvents[e] = events[e];
 			}
 		}
-
 		
 		for (e in currentEvents) {
 			if (currentEvents.hasOwnProperty(e)) {
 				ev =  currentEvents[e];
 				if (ev.delay-- <= 0)  {
-					ev.funv(ev.args);
+					ev.fun(ev.args);
 					that.detach(e);
 				}
 			}
 		}
 		requestAnimationFrame(that.animate);
+		render();
+		stats.update();
 	};
 
 
@@ -60,10 +65,10 @@ CALC.interpolations = {
 		return (Math.sin((t-1/2) * Math.PI) + 1)/2;
 	},
 	cubic: function(t) {
-		return -2*(t^3)+3*(t^2);
+		return (3-2*t)*(t*t);
 	},
 	quintic: function(t) {
-		return 6*(t^5)-15*(t^4)+10*(t^3);
+		return (6*t*t-15*t+10)*(t*t*t);
 	}
 };
 
@@ -71,7 +76,13 @@ CALC.interpolations = {
 
 
 
-var animate = function(object, args, duration, delay, interpolate) {
+CALC.animate = function(object, args, duration, interpolate, delay, callback) {
+
+	for (a in args) {
+		if (args[a] === undefined) delete args[a];
+	}
+	
+	callback = callback || function(){};
 
 	// object[key] ska bli value på duration frames
 	var t = 0;
@@ -90,6 +101,9 @@ var animate = function(object, args, duration, delay, interpolate) {
 		if (t < 1) {
 			CALC.scheduler.attach(step);
 		}
+		else {
+			callback();
+		}
 	};
 
 	var start = function() {
@@ -106,15 +120,31 @@ var animate = function(object, args, duration, delay, interpolate) {
 }
 
 
-
-
-THREE.Object3D.prototype.rotate = function(args, timing) {
+CALC.rotate = function(object, args, timing, callback) {
 	timing = timing || {};
-	animate(this.rotation, {
+	CALC.animate(object.rotation, {
 		x: args.x,
 		y: args.y,
 		z: args.z
-	}, timing.duration, timing.interpolation, timing.delay);
+	}, timing.duration, timing.interpolation, timing.delay, callback);
+	return this;
+};
+
+CALC.translate = function(object, args, timing, callback) {
+	timing = timing || {};
+	CALC.animate(object.position, {
+		x: args.x,
+		y: args.y,
+		z: args.z
+	}, timing.duration, timing.interpolation, timing.delay, callback);
+	return this;
+};
+
+CALC.fade = function(material, args, timing, callback) {
+	timing = timing || {};
+	CALC.animate(material, {
+		opacity: args.opacity
+	}, timing.duration, timing.interpolation, timing.delay, callback);
 	return this;
 };
 
