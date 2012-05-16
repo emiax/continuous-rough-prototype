68de0def3880e03db876f5d34f6e6eb0a1ae4a56
emiax <mail@emilaxelsson.se>
2012-05-16 14:27:22 +0200

    Created separate main file for namespace CALC

--- sandbox/threeTest/src/calculus/expressionTree.js
+++ sandbox/threeTest/src/calculus/expressionTree.js
@@ -1,21 +1,3 @@
-Function.prototype.method = function(name, func) {
-	this.prototype[name] = func;
-	return this;
-};
-
-Function.method('inheritsFrom', function(Parent) {
-	this.prototype = new Parent();
-	this.prototype.constructor = this;
-	this.prototype.super = Parent.prototype;
-	return this;
-});
-
-
-var CALC = function() {
-	var that = {};
-	return that;
-}();
-
 // General Node
 
 CALC.Node = function() {};
