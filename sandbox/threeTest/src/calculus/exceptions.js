5e358d5dd6159bb6e395797cf0dd5f355329db4d
emiax <mail@emilaxelsson.se>
2012-05-22 15:03:22 +0200

    Refactor, moved stuff from sandbox to root. Added concept of "application" and "visualizations"

--- sandbox/threeTest/src/calculus/exceptions.js
+++
@@ -1,9 +0,0 @@
-CALC.InvalidArgumentException = function(message) {
-	this.name = "Invalid argument";
-	this.message = "Invalid argument: " + message;
-}
-
-CALC.NotImplementedYetException = function() {
-	this.name = "Not implemented yet";
-	this.message = "This feature it not yet implemented";
-}
--- sandbox/threeTest/src/calculus/visualizations/tangentialPlane.js
+++
@@ -1,9 +0,0 @@
-CALC.visualizations.TangentialPlane = function() {
-
-
-
-};
-
-
-CALC.visualizations.TangentialPlane.prototype = new CALC.visualizations.Visualization();
-CALC.visualizations.TangentialPlane.prototype.constructor = CALC.visualizations.TangentialPlane;
